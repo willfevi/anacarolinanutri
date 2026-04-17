dfec11f694cbbb2fde20b8d3bad031b2d05f0db1
willfevi <willianfernandesvieira1234@gmail.com>
2026-04-17 16:57:30 -0300

    Fix crn exibição

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 export default function Footer() {
   return (
-    <div className="text-center text-xs italic text-[#a08a96] py-10 border-t border-[#efe7ec]">© {new Date().getFullYear()} Ana Carolina Gonçalves CRN-4 23100301 • Nutricionista Especialista em Oncologia.</div>
+    <div className="text-center text-xs italic text-[#a08a96] py-10 border-t border-[#efe7ec]">© {new Date().getFullYear()} Ana Carolina Gonçalves <strong>CRN-4 23100301</strong> • Nutricionista Especialista em Oncologia.</div>
   );
 }
